554e9c90fecb7910c64571db54164c2a343566f1
TORGBORFRANCISCA <fhrannie92@gmail.com>
2024-09-11 12:42:39 -0700

    q13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,41 +1,43 @@
 #include <stdlib.h>
 #include "lists.h"
+
 /**
  * insert_node - insert node in a sorted list
- * @head: pointer to list to be add
+ * @head: pointer to list to be added
  * @number: number to add
  * Return: the list with the new number
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node;
-	listint_t *tmp = *head;
-/* In case it isn't imput a head*/
-	if (head == NULL)
-		return (NULL);
-/*Allocate the new node*/
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-/*Assign info to the new node*/
-	new_node->n = number;
-	new_node->next = NULL;
-/*Assign the head if the list is empty*/
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
+    listint_t *new_node;
+    listint_t *tmp = *head;
 
-	while (tmp->next != NULL)
-	{
-		if (new_node->n >= tmp->n && new_node->n <= tmp->next->n)
-		{
-			new_node->next = tmp->next;
-			tmp->next = new_node;
-			return (new_node);
-		}
-			tmp = tmp->next;
-	}
-	return (new_node);
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    /* Assign data to the new node */
+    new_node->n = number;
+    new_node->next = NULL;
+
+    /* Insert at the beginning of the list */
+    if (*head == NULL || (*head)->n >= number)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+
+    /* Traverse the list to find the insertion point */
+    while (tmp->next != NULL && tmp->next->n < number)
+    {
+        tmp = tmp->next;
+    }
+
+    /* Insert the new node */
+    new_node->next = tmp->next;
+    tmp->next = new_node;
+
+    return (new_node);
 }
