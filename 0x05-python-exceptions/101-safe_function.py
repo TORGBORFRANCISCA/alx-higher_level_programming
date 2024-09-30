7ae88e25e8269dc703e0d613ae4f744ecff51bc2
TORGBORFRANCISCA <fhrannie92@gmail.com>
2024-09-30 07:52:37 -0700

    ./101-main.py 2> /dev/null

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-from sys import stderr
+
 def safe_function(fct, *args):
+    """ Safely call a function
+
+    Args:
+        fct (function pointer): function to be called
+        args (list): function arguments
+
+    Returns:
+        fct's result, OR None if something goes wrong
+    """
     try:
-        res = fct(*args)
-        return (res)
-    except Exception as err:
-        stderr.write('Exception: {}\n'.format(err))
-        return (None)
+        return fct(*args)
+    except Exception as ex:
+        import sys
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
